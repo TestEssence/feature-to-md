6eff4e74b2fd97df4dd64ed626b92dce6d53ce16
anatoly.sakhno <anatoly.sakhno@gmail.com>
2023-05-01 22:45:50 +0300

    Add a command line param to turn debug output on/off Fixes #6

--- src/test/api.spec.ts
+++ src/test/api.spec.ts
@@ -2,9 +2,25 @@ import { featureToMd } from '..';
 import test from 'ava';
 import { readFileSync } from 'fs';
 import {basename, resolve} from "path";
+import { Config } from '../lib/config';
 
 test('convert the feature example to markdown @api', async (t) => {
     const markdown = await featureToMd({ path: resolve(__dirname, 'features', 'example.feature') });
+    t.is(basename(markdown.filename!), 'example.md');
+    t.truthy(markdown.content);
+    t.notThrows(() => readFileSync(resolve(__dirname, 'features', 'example.feature'), 'utf-8'));
+});
+
+test('convert the feature example to markdown with debug log @api', async (t) => {
+    let config: Config = {
+        featureSummaryTemplate: "",
+        scenarioFooterTemplate: "",
+        feature_file_encoding: "utf-8",
+        targetDir: "",
+        debugMode: true,
+    };
+
+    const markdown = await featureToMd({ path: resolve(__dirname, 'features', 'example.feature') }, config );
     t.is(basename(markdown.filename!), 'example.md');
     t.truthy(markdown.content);
     t.notThrows(() => readFileSync(resolve(__dirname, 'features', 'example.feature'), 'utf-8'));
